1ddee0f12257e1435098cdfeb31af1dfafdac5bb
fredli4qooni <fredlifourqoni999@gmail.com>
2026-08-29 15:24:37 +0700

    i18n: terjemahkan pesan error validasi login kredensial ke bahasa indonesia

---
+++ lang/id/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Otentikasi
+    |--------------------------------------------------------------------------
+    */
+
+    'failed' => 'Email atau kata sandi yang Anda masukkan salah.',
+    'password' => 'Kata sandi yang dimasukkan tidak sesuai.',
+    'throttle' => 'Terlalu banyak percobaan masuk. Silakan coba lagi dalam :seconds detik.',
+
+];
